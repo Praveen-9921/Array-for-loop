4614ad44f7d6ec5c2b1e3d69a9a37be087a06520
Praveen-9921 <121763099+Praveen-9921@users.noreply.github.com>
2023-01-24 23:27:06 +0530

    Add files via upload

---
+++ Array/array.js
@@ -0,0 +1,126 @@
+//length of array(It help to know the length of the array)
+//let x=["Apple", "Mango", "Banana", "Grapes", "orange"];
+//console.log(x.length)
+
+
+
+
+
+//Push in array(Add text in last of the array)
+// let x=["Apple", "Mango", "Banana", "Grapes", "orange"];
+// x.push("Praveen")
+// console.log(x)
+
+
+
+
+//unshift in array(Add text in Start of the array)
+// let x=["Apple", "Mango", "Banana", "Grapes", "orange"];
+// x.unshift("aditya")
+// console.log(x)
+
+
+
+// POP in array(It remove the element from the end)
+// let x=["Apple", "Mango", "Banana", "Grapes", "orange"];
+// x.pop()
+// console.log(x)
+
+
+
+
+// POP in array(It remove the element from the start)
+// let x=["Apple", "Mango", "Banana", "Grapes", "orange"];
+// x.shift()
+// console.log(x)
+
+
+
+
+/*Splice in array(It helps to remove the element from the any place
+(First value is start point,and second value is how many elements you delete ))*/
+// let x=["Apple", "Mango", "Banana", "Grapes", "orange"];
+// x.splice(2,2)
+// console.log(x)
+
+
+
+
+//Here length help to print the value to your requirement
+// let x=["Apple", "Mango", "Banana", "Grapes", "orange"];
+// x.length=2;
+// console.log(x)
+
+
+
+
+
+
+//indexof in array(it helps to find the index of any element present in array)
+// let x=["Apple", "Mango", "Banana", "Grapes", "orange"];
+// let y=x.indexOf("Apple");
+// console.log(y)
+
+
+
+
+
+
+//split in array(it change the sentence into array)
+// let x = "My name is Praveen Kumar";
+// let y =x.split(" ")
+// console.log(y)
+
+
+
+
+//join in array(It chage the array into sentence)
+// let x=["Apple", "Mango", "Banana", "Grapes", "orange"];
+// let y = x.join(" ")
+// console.log(y)
+
+
+
+
+//concat in array(It helps to join multiple array)
+// let x=["Apple", "Mango", "Banana", "Grapes", "orange"];
+// let y=["Cherry","Pineapple"]
+// let z= x.concat(y);
+// console.log(z);
+
+
+
+
+
+                                    //Loop
+
+
+//for loop
+
+// for(i=0;i<10;i++){
+//     console.log(i)
+// }
+
+
+
+
+let x=[];
+for(i=0;i<=100;i++){
+    x.push("Praveen");
+}
+console.log(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
