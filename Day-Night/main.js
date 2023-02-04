6bcbc4a768cd89b44fc9a242ea2006211fe7222e
Praveen-9921 <121763099+Praveen-9921@users.noreply.github.com>
2023-02-04 23:16:12 +0530

    Add files via upload

---
+++ Day-Night/main.js
@@ -0,0 +1,28 @@
+// let a =[]
+// for(i=0;i<=100;i++){
+//     a.push("Praveen"+i)
+// }
+// console.log(a)
+
+// let b =a.length
+// console.log(b)
+
+// let c ="I AM A PRAVEEN"
+// let d=c.indexOf("PRAVEEN")
+// let e=c.length
+// console.log(d)
+// console.log(e)
+
+
+
+// let day = parseInt(prompt("Enter your time"))
+// let daynight =prompt("Enter am or pm")
+// if(day<12 && daynight=="am"){
+//     console.log("Good Morning")
+// }
+// else if(day<12 && daynight=="pm"){
+//     console.log("Good Night")
+// }
+// else{
+//     console.log("Please enter a valid input")
+// }
